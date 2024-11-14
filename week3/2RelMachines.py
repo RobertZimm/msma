30ff2447e1cfbbb88922b0ea40aae95bdaac6eb3
Robert Zimmermann <rz2000@gmx.de>
2024-11-14 11:44:47 +0100

    added singularity test

--- week3/2RelMachines.py
+++ week3/2RelMachines.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def two_rel_machines(mu_1: float, mu_2: float, C: int, runtime: int):
     state_times = [0 for _ in range(C+3)]
-    TH_i = [0 for _ in range(2)] # 
+    TH_i = [0 for _ in range(2)]
     t = 0
     current_state = 0
 
@@ -46,23 +46,57 @@ def two_rel_machines(mu_1: float, mu_2: float, C: int, runtime: int):
                 current_state -= 1
 
     TH_i[0] /= t
-    TH_i[1] /= t
-    # TH_i[1] = TH_i[0] * (mu_1/mu_2)
+    # TH_i[1] /= t
+    TH_i[1] = (mu_1/mu_2) * TH_i[0]
 
     for i in range(len(state_times)):
         state_times[i] /= t
 
     n_bar = sum([i * state_times[i] for i in range(len(state_times))])
+    # TH_i = [calc_TH1(C, mu_1, mu_2, state_times), calc_TH2(C, mu_1, state_times)]
 
     return n_bar, state_times, TH_i
 
 
-def calc_pi(C: int, mu_1: float, mu_2: float):
-    pi = np.zeros(C+3)
-    pi[0] = (1-(mu_1/mu_2))/(1-(mu_1/mu_2)**(C+3))
+def calc_Q(C: int, mu_1: float, mu_2: float):
+    Q = np.zeros((C+3, C+3))
 
-    for i in range(1, C+3):
-        pi[i] = (mu_1/mu_2)**i * pi[0]
+    Q[0, 0] = -mu_1
+    Q[0, 1] = mu_1
+
+    Q[C+2, C+2] = -mu_2
+    Q[C+2, C+1] = mu_2
+
+    for i in range(1, C+2):
+        Q[i, i-1] = mu_1
+        Q[i, i] = -mu_1 - mu_2
+        Q[i, i+1] = mu_2
+
+    return Q
+
+
+def test_Q_sing(C: int, mu_1: float, mu_2: float, eps: float = 1e-10):
+    Q = calc_Q(C, mu_1, mu_2)
+
+    if np.abs(np.linalg.det(Q)) < eps:
+        print("Q is singular. System is underdetermined. ")
+        return True
+
+    else:
+        print("Q is not singular")
+        return False
+        
+
+def calc_pi(C: int, mu_1: float, mu_2: float):
+    if mu_1 != mu_2:
+        pi = np.zeros(C+3)
+        pi[0] = (1-(mu_1/mu_2))/(1-(mu_1/mu_2)**(C+3))
+
+        for i in range(1, C+3):
+            pi[i] = (mu_1/mu_2)**i * pi[0]
+
+    else:
+        pi = (1/(C+3)) * np.ones(C+3)
 
     return pi
 
@@ -85,13 +119,13 @@ if __name__ == "__main__":
     mu_1 = 0.3
     mu_2 = 0.6
 
-    pi = calc_pi(C, mu_1, mu_2)
-    n_bar = calc_n_bar(pi)
-    TH_i = [calc_TH1(C, mu_1, mu_2, pi), calc_TH2(C, mu_1, pi)]
+    # pi = calc_pi(C, mu_1, mu_2)
+    # n_bar = calc_n_bar(pi)
+    # test = [calc_TH1(C, mu_1, mu_2, pi), calc_TH2(C, mu_1, pi)]
 
-    n_bar_exp, pi_exp, TH_i_exp = two_rel_machines(mu_1, mu_2, C, 1e5)
+    # n_bar_exp, pi_exp, TH_i_exp = two_rel_machines(mu_1, mu_2, C, 1e5)
 
-    print(n_bar, n_bar_exp)
-    print(pi, pi_exp)
-    print(TH_i, TH_i_exp)
-    print(calc_TH1(C, mu_1, mu_2, pi_exp), calc_TH2(C, mu_1, pi_exp))
+    # print(n_bar, n_bar_exp)
+    # print(pi, pi_exp)
+    # print(test, TH_i_exp)
+    test_Q_sing(C, mu_1, mu_2)
