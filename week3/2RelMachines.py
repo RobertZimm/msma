fc00749de795f3eb282b9e890c47bc9492431283
Robert Zimmermann <rz2000@gmx.de>
2024-11-14 13:56:06 +0100

    added singularity test

--- week3/2RelMachines.py
+++ week3/2RelMachines.py
@@ -4,7 +4,6 @@ import numpy as np
 
 def two_rel_machines(mu_1: float, mu_2: float, C: int, runtime: int):
     state_times = [0 for _ in range(C+3)]
-    TH_i = [0 for _ in range(2)]
     t = 0
     current_state = 0
 
@@ -13,15 +12,13 @@ def two_rel_machines(mu_1: float, mu_2: float, C: int, runtime: int):
             draw = np.random.exponential(1/mu_1)
             t += draw
             state_times[current_state] += draw
-            TH_i[0] += 1
             current_state += 1
         
         elif current_state == C+2:
             draw = np.random.exponential(1/mu_2)
             t += draw
             state_times[current_state] += draw
-            TH_i[0] += 1
-            # TH_i[1] += 1
+
             current_state -= 1
 
         else:
@@ -32,28 +29,21 @@ def two_rel_machines(mu_1: float, mu_2: float, C: int, runtime: int):
                 t += draw_mu_1
                 state_times[current_state] += draw_mu_1
 
-                if current_state <= C:
-                    TH_i[0] += 1
-
                 current_state += 1
 
             else:
                 t += draw_mu_2
                 state_times[current_state] += draw_mu_2
-                
-                # TH_i[1] += 1
 
                 current_state -= 1
 
-    TH_i[0] /= t
-    # TH_i[1] /= t
-    TH_i[1] = (mu_1/mu_2) * TH_i[0]
+    # count_TH_i[1] = (mu_1/mu_2) * count_TH_i[0]
 
     for i in range(len(state_times)):
         state_times[i] /= t
 
     n_bar = sum([i * state_times[i] for i in range(len(state_times))])
-    # TH_i = [calc_TH1(C, mu_1, mu_2, state_times), calc_TH2(C, mu_1, state_times)]
+    TH_i = [calc_TH1(C, mu_1, mu_2, state_times), calc_TH2(C, mu_1, state_times)]
 
     return n_bar, state_times, TH_i
 
@@ -72,6 +62,8 @@ def calc_Q(C: int, mu_1: float, mu_2: float):
         Q[i, i] = -mu_1 - mu_2
         Q[i, i+1] = mu_2
 
+    print(Q)
+
     return Q
 
 
@@ -85,7 +77,7 @@ def test_Q_sing(C: int, mu_1: float, mu_2: float, eps: float = 1e-10):
     else:
         print("Q is not singular")
         return False
-        
+
 
 def calc_pi(C: int, mu_1: float, mu_2: float):
     if mu_1 != mu_2:
@@ -113,19 +105,19 @@ def calc_TH2(C: int, mu_1: float, pi: list):
     return mu_1 * sum(pi[1:C+3])
 
 
-
 if __name__ == "__main__":
     C = 2
     mu_1 = 0.3
     mu_2 = 0.6
 
-    # pi = calc_pi(C, mu_1, mu_2)
-    # n_bar = calc_n_bar(pi)
-    # test = [calc_TH1(C, mu_1, mu_2, pi), calc_TH2(C, mu_1, pi)]
+    # test_Q_sing(C, mu_1, mu_2)
 
-    # n_bar_exp, pi_exp, TH_i_exp = two_rel_machines(mu_1, mu_2, C, 1e5)
+    pi = calc_pi(C, mu_1, mu_2)
+    n_bar = calc_n_bar(pi)
+    test = [calc_TH1(C, mu_1, mu_2, pi), calc_TH2(C, mu_1, pi)]
 
-    # print(n_bar, n_bar_exp)
-    # print(pi, pi_exp)
-    # print(test, TH_i_exp)
-    test_Q_sing(C, mu_1, mu_2)
+    n_bar_exp, pi_exp, TH_i_exp = two_rel_machines(mu_1, mu_2, C, 1e5)
+
+    print(n_bar, n_bar_exp)
+    print(pi, pi_exp)
+    print(test, TH_i_exp)
