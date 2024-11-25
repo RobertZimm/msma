7a7e567f1c7cb89457a014129b803845ff356888
Robert Zimmermann <rz2000@gmx.de>
2024-11-25 11:54:24 +0100

    cleaned up 2machinebothrel

--- two_reliable_machines/TwoMachineLineBothReliableClassroom2024.py
+++ two_reliable_machines/TwoMachineLineBothReliableClassroom2024.py
@@ -22,6 +22,8 @@ class TwoMachineLineBothReliable:
         for n in range(self.NumberOfStates):
             counter += 1
             self.StN[n] = counter  # so states are numbered from 0
+            
+        self.determineSteadyStateProbabilities()
 
     def initializeGeneratorMatrix(self):
         for n in range(self.NumberOfStates):
@@ -35,11 +37,41 @@ class TwoMachineLineBothReliable:
                 if i != j:
                     self.Q[i][i] = self.Q[i][i] - self.Q[i][j]
 
+    def determineSteadyStateProbabilities(self):
+        self.initializeGeneratorMatrix()
+        self.Qmod = self.Q.copy()
+        numberOfRowsOfQ = self.Qmod.shape[0]
+        for i in range(numberOfRowsOfQ):
+            self.Qmod[i][numberOfRowsOfQ - 1] = 1  # Qmod is quadratic
+        # right hand side row vector with last element as a 1
+        self.nmod[0][numberOfRowsOfQ - 1] = 1
+
+        self.pi = self.nmod.dot(la.inv(self.Qmod))
+        
+    
+    def calc_TH1(self):
+        return self.mu1 * (1 - self.pi[0][self.StN[self.N]])
+        
+    
+    def calc_TH2(self):
+        return self.mu2 * (1 - self.pi[0][self.StN[0]])
+        
+    def calc_n_bar(self):
+        return sum([i * self.pi[0][i] for i in range(self.NumberOfStates)])
+
 
 # We now create an object of the class
-
 myTwoMachineLine = TwoMachineLineBothReliable("StefansLine", 10, 8, 2)
 
-myTwoMachineLine.initializeGeneratorMatrix()
-
 print(myTwoMachineLine.Q)
+print(myTwoMachineLine.Qmod)
+print("Vector of state probablities is:", myTwoMachineLine.pi)
+
+print("Throughput via Machine 1 is:",
+    myTwoMachineLine.calc_TH1(),)
+
+print("Throughput via Machine 2 is:",
+    myTwoMachineLine.calc_TH2(),)
+
+print("Average parts in the system is:", 
+      myTwoMachineLine.calc_n_bar())
