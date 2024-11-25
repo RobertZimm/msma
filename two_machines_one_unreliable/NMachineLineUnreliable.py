67cac84a5b02372e395add1975ac942d879b699a
Robert Zimmermann <rz2000@gmx.de>
2024-11-25 17:03:33 +0100

    added some functionality for r and p

--- two_machines_one_unreliable/NMachineLineUnreliable.py
+++ two_machines_one_unreliable/NMachineLineUnreliable.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 
-def num_analysis(mu, C, sim_duration):
-    # Definition of codes (numbers) for events
-    process_step_completed = 1
-    machine_failure = 2
-    machine_repair = 3
+
+# Definition of codes (numbers) for events
+PROCESS_STEP_COMPLETED = 1
+MACHINE_FAILURE = 2
+MACHINE_REPAIR = 3
+
+
+def num_analysis(mu, r, p, C, sim_duration):
+    # Initialize the random number generator with a given seed.
+    rng = np.random.default_rng(4711)
 
     num_machines = len(mu)
 
@@ -14,23 +19,30 @@ def num_analysis(mu, C, sim_duration):
 
     trans_time = sim_duration/10
 
+    # Initialize the state variables
     parts_processed = np.zeros(num_machines, 
                              dtype=int)
     ext_buffer_level = np.zeros(num_machines - 1, 
-                                   dtype=int)
+                                dtype=int)
     time_until_next_part = np.zeros(num_machines, 
-                                                dtype=float)
-
-    # First machine starts with a workpiece, all other machines idle and all buffers
-    # are empty
-    # Initialize the random number generator with a given seed.
-    rng = np.random.default_rng(4711)
+                                    dtype=float)
+    
+    machine_state = np.ones(num_machines, 
+                             dtype=int)
+    
+    # If machine up: time until failure
+    # If machine down: time until repair
+    time_until_state_change = np.zeros(num_machines, 
+                                 dtype=float)
 
     # Processing time of the first workpiece on the first machine ( offset 0 )
-    time_until_next_part[0] = rng.exponential(1 / mu[0])
+    time_until_next_part[0] = rng.exponential(1/mu[0])
 
     # Initialize the other machines with plus infinity
     time_until_next_part[1:] = np.inf
+
+    for i in range(num_machines):
+        time_until_state_change[i] = rng.exponential(1/p[i])
 
     # Start simulation of this Markovian system
     sim_clock = -trans_time
@@ -38,25 +50,35 @@ def num_analysis(mu, C, sim_duration):
     while sim_clock < sim_duration:
         time_until_next_event = np.inf
         next_machine = np.inf
-        next_event_type = 0  # no such event exists
 
-        for machine_number in range(num_machines):
-            if isProductionReady(machine_number, 
-                                 ext_buffer_level, 
-                                 C, 
-                                 num_machines):
-                if time_until_next_part[
-                    machine_number] < time_until_next_event:
-                    time_until_next_event = time_until_next_part[
-                                                                machine_number]
-                    next_machine = machine_number  # next event at this current machine i
-                    next_event_type = process_step_completed
+        # Choose initial event which does not exist
+        next_event_type = 0
+
+        # Get production ready machine 
+        # with smallest time until next part
+        for n in range(num_machines):
+            if (is_prod_ready(n, ext_buffer_level, 
+                             C, num_machines,
+                             machine_state)
+                and time_until_next_part[n] < time_until_next_event):
+                time_until_next_event = time_until_next_part[n]
+                next_machine = n 
+                next_event_type = PROCESS_STEP_COMPLETED
+
+            if time_until_next_part[n] < time_until_next_event:
+                time_until_next_event = time_until_state_change[n]
+                next_machine = n
+
+                if machine_state[n] == 1:
+                    next_event_type = MACHINE_FAILURE     
+                else:
+                    next_event_type = MACHINE_REPAIR
 
         # Advance in time
         sim_clock += time_until_next_event
 
         # Execute the next event
-        if next_event_type == process_step_completed:
+        if next_event_type == PROCESS_STEP_COMPLETED:
             if next_machine == 0:
                 ext_buffer_level[next_machine] += 1
 
@@ -67,31 +89,45 @@ def num_analysis(mu, C, sim_duration):
                 ext_buffer_level[next_machine - 1] -= 1
                 ext_buffer_level[next_machine] += 1
 
+            # Transient phase is over, we begin to count the processed parts
             if sim_clock > 0:
-                # Transient phase is over, we begin to count the processed parts
                 parts_processed[next_machine] += 1
+
+        elif next_event_type == MACHINE_FAILURE:
+            machine_state[next_machine] = 0
+
+        elif next_event_type == MACHINE_REPAIR:
+            machine_state[next_machine] = 1
 
         # Given the new state, and USING THE MEMORYLESSNESS PROPERTY, we update
         # the times until the next events. Since it is a CTMC, we do not need an
         # event calender.
-        for machine_number in range(num_machines):
-            if isProductionReady(machine_number, 
-                                 ext_buffer_level, 
-                                 C, 
-                                 num_machines):
-                # This is for machines that are neither blocked nor starved
-                time_until_next_part[
-                    machine_number] = rng.exponential(1/mu[machine_number])
-                
+        for n in range(num_machines):
+            # This is for machines that are neither blocked nor starved
+            if is_prod_ready(n, ext_buffer_level, 
+                             C, num_machines,
+                             machine_state):
+                time_until_next_part[n] = rng.exponential(1/mu[n])
+            
+            # This is for machines that are blocked or starved
             else:
-                time_until_next_part[machine_number] = np.inf
+                time_until_next_part[n] = np.inf
 
+            if machine_state[n] == 1:
+                time_until_state_change[n] = rng.exponential(1/p[n])
+            else:
+                time_until_state_change[n] = rng.exponential(1/r[n])
+
+    # Calculate throughput 
     th = parts_processed / sim_duration
 
     return th, parts_processed
 
 
-def isProductionReady(machine_num, ext_buffer_level, C, num_machines):
+def is_prod_ready(machine_num, ext_buffer_level, C, num_machines, machine_state):
+    if machine_state[machine_num] == 0:
+        return False
+    
     return (machine_num == 0 and ext_buffer_level[machine_num] < C[machine_num] + 2
                 or 
                 machine_num == num_machines - 1 and ext_buffer_level[machine_num - 1] > 0
@@ -100,12 +136,13 @@ def isProductionReady(machine_num, ext_buffer_level, C, num_machines):
                 and ext_buffer_level[machine_num] < C[machine_num] + 2)
 
 
-
 if __name__ == "__main__":
+    r = np.array([0.3, 0.3, 0.3]) # rate of repair
+    p = np.array([0.1, 0.1, 0.1]) # rate of failure
     mu = np.array([10, 8, 8]) # rate of completion of machines
     C = np.array([1000, 1000]) # Size of buffers between machines
     sim_duration = 10000 # time to be simulated
 
-    th, parts_processed = num_analysis(mu, C, sim_duration)
+    th, parts_processed = num_analysis(mu, r, p, C, sim_duration)
     print(th)
     print(parts_processed)
